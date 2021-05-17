4c54253abea81540362a4122e2032a59999db770
Synth <synth9283@gmail.com>
2021-05-17 12:24:53 -0400

    Implemented command to get icon as image

---
+++ src/commands/icon.c
@@ -0,0 +1,19 @@
+#include <orca/discord.h>
+#include "../libs/get_icon_url.h"
+
+void icon(struct discord *client, const struct discord_user *user, const struct discord_message *msg) {
+    if (msg->author->bot) return;
+
+    struct discord_embed *embed = discord_embed_alloc();
+    struct discord_create_message_params params = {.embed = embed};
+
+    embed->timestamp = orka_timestamp_ms();
+    char *icon_url = get_icon_url(msg->author);
+    discord_embed_set_author(embed, msg->author->username, NULL, icon_url, NULL);
+    discord_embed_set_image(embed, icon_url, NULL, 64, 64);
+    
+    discord_create_message(client, msg->channel_id, &params, NULL);
+    free(icon_url);
+
+    return;
+}
---
+++ src/libs/icon.h
@@ -0,0 +1,4 @@
+#include <orca/discord.h>
+#include "../libs/get_icon_url.h"
+
+void icon(struct discord *client, const struct discord_user *user, const struct discord_message *msg);
