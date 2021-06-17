7fa9a0e58f51cae32c96c1d717e75da1400cfb58
Synth <synth9283@gmail.com>
2021-06-17 14:33:09 -0400

    Fixed bot's deleted message logging system

--- src/events/on_message_delete.c
+++ src/events/on_message_delete.c
@@ -6,18 +6,17 @@
 #include "../libs/remove_message_db.h"
 
 void on_message_delete(struct discord *client, const struct discord_user *bot, const u64_snowflake_t message_id, const u64_snowflake_t channel_id, const u64_snowflake_t guild_id) {
-    // Not working
     struct discord_embed *embed = discord_embed_alloc();
     struct discord_create_message_params params = {.embed = embed};
-    struct discord_message *message; // temp replacement for the line below
-    // struct discord_messsage *message = fetch_message_db(client, guild_id, message_id);
+    struct discord_message *message = fetch_message_db(client, guild_id, message_id);
+    puts(message->content);
     embed->color = COLOR_RED;
     embed->timestamp = orka_timestamp_ms();
 
     if (message) {
-        // char *icon_url = get_icon_url(message->author);
-        // discord_embed_set_author(embed, message->author->username, NULL, icon_url, NULL);
-        // discord_embed_set_thumbnail(embed, icon_url, NULL, AVATAR_HEIGHT, AVATAR_WIDTH);
+        char *icon_url = get_icon_url(message->author);
+        discord_embed_set_author(embed, message->author->username, NULL, icon_url, NULL);
+        discord_embed_set_thumbnail(embed, icon_url, NULL, AVATAR_HEIGHT, AVATAR_WIDTH);
 
         snprintf(embed->title, 257, "Deleted message by %s#%s", message->author->username, message->author->discriminator);
         char message_id_str[19], channel_id_str[19];
@@ -29,9 +28,8 @@ void on_message_delete(struct discord *client, const struct discord_user *bot, c
         snprintf(embed->footer->text, 2049, "Author ID: %lu", message->author->id);
 
         discord_create_message(client, C_LOG, &params, NULL);
-        // free(icon_url);
 
-        discord_create_message(client, C_LOG, &params, NULL);
+        free(icon_url);
         discord_message_free(message);
         discord_embed_free(embed);
 
--- src/other/fetch_message_db.c
+++ src/other/fetch_message_db.c
@@ -13,20 +13,26 @@ struct discord_message *fetch_message_db(struct discord *client, u64_snowflake_t
     else {
         char *query = NULL, *errMsg = NULL;
         query = sqlite3_mprintf("SELECT timestamp, author_id, message_id, content FROM %s WHERE message_id = %lu;", MESSAGE_TABLE, message_id);
-        sqlite3_prepare16_v2(db, query, -1, &stmt, NULL);
+        puts(query);
+        rc = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+
         while ((rc = sqlite3_step(stmt)) != SQLITE_DONE) {
             u64_snowflake_t db_message_id = 0;
-            struct discord_user *member = discord_user_alloc();
+            printf("Timestamp: %lu, Author_id: %lu, Message_id: %lu, Content: %s\n", sqlite3_column_int64(stmt, 0), sqlite3_column_int64(stmt, 1), sqlite3_column_int64(stmt, 2), sqlite3_column_text(stmt, 3));
             if ((db_message_id = sqlite3_column_int64(stmt, 2)) == message_id) {
+                printf("%lu, %lu\n", db_message_id, message_id);
+                struct discord_user *member = discord_user_alloc();
                 message->timestamp = sqlite3_column_int64(stmt, 0);
                 discord_get_guild_member(client, guild_id, db_message_id, (struct discord_guild_member*)member);
                 message->author = member;
                 message->author->id = sqlite3_column_int64(stmt, 1);
                 message->id = db_message_id;
-                strcpy(message->content, (char*)sqlite3_column_value(stmt, 3));
+                message->content = malloc(4001);
+                strcpy(message->content, sqlite3_column_text(stmt, 3));
             }
         }
         sqlite3_free(query);
+
         if (rc != SQLITE_OK) {
             printf("\nSQL Error: %s\n\n", errMsg);
             sqlite3_free(errMsg);
