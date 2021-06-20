204b90b244bfd838a980ab4e8067d9c1e565d5d7
Synth <synth9283@gmail.com>
2021-06-20 16:49:40 -0400

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ int main() {
     struct discord *client = discord_config_init("bot.config");
 
     discord_set_on_ready(client, &on_ready);
-    discord_set_on_voice_state_update(client, &on_voice_state_update);
     discord_set_on_message_create(client, &on_message_create);
     discord_set_on_message_delete(client, &on_message_delete);
     discord_set_on_message_update(client, &on_message_update);
@@ -14,6 +13,8 @@ int main() {
     discord_set_on_message_reaction_remove(client, &on_message_reaction_remove);
     discord_set_on_guild_member_add(client, &on_guild_member_add);
     discord_set_on_guild_member_remove(client, &on_guild_member_remove);
+    discord_set_on_voice_state_update(client, &on_voice_state_update);
+
     discord_set_on_command(client, "help", &on_help);
     discord_set_on_command(client, "ping", &on_ping);
     discord_set_on_command(client, "icon", &on_icon);
