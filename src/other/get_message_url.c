b193d1559715ff84f68b878b659fc18cb9793320
Synth <synth9283@gmail.com>
2021-06-21 02:51:46 -0400

    Unused import

--- src/other/get_message_url.c
+++ src/other/get_message_url.c
@@ -1,9 +1,9 @@
 #include <orca/discord.h>
-#include <orca/cee-utils.h>
 #include "../libs/bot_include.h"
 
 void get_message_url(char buf[], const struct discord_message *message) {
     // buf[MESSAGE_URL_LEN]
+    memset(buf, 0, MESSAGE_URL_LEN);
     snprintf(buf, MESSAGE_URL_LEN, "https://discord.com/channels/%lu/%lu/%lu", message->guild_id, message->channel_id, message->id);
 
     return;
