0b6580ac98375a9bf016bdd801e2b8a39d20000b
Synth <synth9283@gmail.com>
2021-06-24 17:56:02 -0400

    Got adding and removing by role_id working

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 target = main
-compiler = gcc
+cc = gcc
 src = $(wildcard src/*/*.c)
 obj = $(src:.c=.o)
-flags = -O2 -ldiscord -lcurl -lcrypto -lssl -lm -lpthread -pthread -lsqlite3 `pkg-config --cflags --libs json-c`
+flags = -g -ldiscord -lcurl -lcrypto -lssl -lm -lpthread -pthread -lsqlite3 `pkg-config --cflags --libs json-c`
 all: $(target)
 
 $(target): main.c $(obj)
-	$(compiler) $^ $(flags) -o $@
+	$(cc) $^ $(flags) -o $@
 
 %.o: %.c
-	$(compiler) -c $(flags) $^ -o $@
+	$(cc) -c $(flags) $^ -o $@
 
 clean:
 	rm -rfv $(obj) $(target)
--- src/commands/add_role_all_user.c
+++ src/commands/add_role_all_user.c
@@ -7,7 +7,6 @@ void add_role_all_user(struct discord *client, const struct discord_user *bot, c
     char author_avatar_url[AVATAR_URL_LEN];
     struct discord_guild_member *guild_member = discord_guild_member_alloc();
     struct discord_embed *embed = discord_embed_alloc();
-    struct discord_guild *guild = discord_guild_alloc();
 
     embed->timestamp = msg->timestamp;
     get_avatar_url(author_avatar_url, msg->author);
@@ -51,18 +50,12 @@ void add_role_all_user(struct discord *client, const struct discord_user *bot, c
             return;
         }
         else {
-            size_t args_count = get_args_len(msg, " ");
-            u64_snowflake_t args_ids_int[args_count];
             char args_id_str[ID_STR_LEN], role_mention_str[ROLE_MENTION_LEN];
-            // struct discord_guild *guild = discord_guild_alloc(); had to move to line 10 to prevent dbus
-            struct discord_message *embed_message = NULL;
-            struct discord_create_message_params params = {.embed = embed};
-            struct discord_edit_message_params edit_params = {.embed = embed};
+            struct discord_guild *guild = discord_guild_alloc();
+            struct discord_message *embed_message = discord_message_alloc();
             struct discord_list_guild_members_params list_guild_members_params = {.limit = 1000};
             struct discord_guild_preview guild_preview;
 
-            // deletes client
-            get_args_ids_as_int(args_ids_int, msg, " ");
             discord_get_guild(client, msg->guild_id, guild);
             discord_list_guild_members(client, msg->guild_id, &list_guild_members_params, &guild->members);
             discord_get_guild_preview(client, msg->guild_id, &guild_preview);
@@ -71,13 +64,18 @@ void add_role_all_user(struct discord *client, const struct discord_user *bot, c
             snprintf(embed->title, 257, "Please wait...");
             snprintf(embed->description, 2049, "Adding roles to %d members", guild_preview.approximate_member_count);
 
-            for (size_t i=0; i<=args_count; i++) {
-                printf("i: %lu : %lu", i, args_ids_int[i]);
+            size_t args_count = get_args_len(msg, " ");
+            u64_snowflake_t *args_ids_int = calloc(args_count, sizeof(u64_snowflake_t));
+            get_args_ids_as_int(args_ids_int, msg, " ");
+
+            for (size_t i=0; args_ids_int[i]; i++) {
+                get_args_ids_as_str_at(args_id_str, msg, i, " ");
                 role_mention(role_mention_str, args_ids_int[i]);
 
                 discord_embed_add_field(embed, args_id_str, role_mention_str, false);
             }
 
+            struct discord_create_message_params params = {.embed = embed};
             discord_create_message(client, msg->channel_id, &params, embed_message);
 
             for (size_t i=0; args_ids_int[i]; i++) {
@@ -92,11 +90,12 @@ void add_role_all_user(struct discord *client, const struct discord_user *bot, c
             snprintf(embed->title, 257, "Done! Took %lu ms", cee_timestamp_ms()-msg->timestamp);
             snprintf(embed->description, 2049, "Added roles to %d members", guild_preview.approximate_member_count);
 
+            struct discord_edit_message_params edit_params = {.embed = embed};
             discord_edit_message(client, msg->channel_id, embed_message->id, &edit_params, NULL);
 
+            free(args_ids_int);
             discord_guild_free(guild);
             discord_embed_free(embed);
-            discord_message_free(embed_message);
         }
     }
 
