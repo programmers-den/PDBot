9d2507115777f91eab56cde6d23daa451fc61ff4
Synth <synth9283@gmail.com>
2021-07-02 23:35:08 -0400

    Fixed segfault

--- src/events/on_message_update.c
+++ src/events/on_message_update.c
@@ -11,13 +11,6 @@ void on_message_update(struct discord *client, const struct discord_user *bot, c
     struct discord_message *db_message = fetch_message_db(client, message->guild_id, message->id);
 
     get_avatar_url(author_avatar_url, message->author);
-    id_to_str(message_id_str, message->id);
-    message_mention(message_str, "Jump to message", message);
-    id_to_str(channel_id_str, message->channel_id);
-    channel_mention(channel_str, message->channel_id);
-    id_to_str(author_id_str, message->author->id);
-    user_mention(author_str, message->author->id);
-    username_and_discriminator_to_str(username_and_discriminator, message->author);
 
     if (db_message->content[0]) update_message_db(message);
     else add_message_db(message);
@@ -27,14 +20,21 @@ void on_message_update(struct discord *client, const struct discord_user *bot, c
     discord_embed_set_author(embed, message->author->username, NULL, author_avatar_url, NULL);
     discord_embed_set_thumbnail(embed, author_avatar_url, NULL, AVATAR_HEIGHT, AVATAR_WIDTH);
     snprintf(embed->footer->text, 2049, "Author ID: %lu", message->author->id);
+    username_and_discriminator_to_str(username_and_discriminator, message->author);
     snprintf(embed->title, 257, "Edit message by %s", username_and_discriminator);
+    id_to_str(message_id_str, message->id);
     discord_embed_add_field(embed, "Message ID", message_id_str, true);
+    id_to_str(channel_id_str, message->channel_id);
     discord_embed_add_field(embed, "Channel ID", channel_id_str, true);
+    id_to_str(author_id_str, message->author->id);
     discord_embed_add_field(embed, "Author ID", author_id_str, true);
+    message_mention(message_str, "Jump to message", message);
     discord_embed_add_field(embed, "Message", message_str, false);
+    channel_mention(channel_str, message->channel_id);
     discord_embed_add_field(embed, "Channel", channel_str, true);
+    user_mention(author_str, message->author->id);
     discord_embed_add_field(embed, "Author", author_str, true);
-    discord_embed_add_field(embed, "Previous content", db_message->content, false);
+    if (!db_message->content[0]) discord_embed_add_field(embed, "Previous content", db_message->content, false);
     discord_embed_add_field(embed, "New content", message->content, false);
 
     discord_create_message(client, C_LOG, &params, NULL);
