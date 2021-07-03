2ed2b4367406a7910f468e9178477114748aad09
Synth <synth9283@gmail.com>
2021-07-03 00:35:03 -0400

    Fixed vc roles updating when not leaving

--- src/events/on_voice_state_update.c
+++ src/events/on_voice_state_update.c
@@ -2,7 +2,8 @@
 #include "../libs/bot_include.h"
 
 void on_voice_state_update(struct discord *client, const struct discord_user *bot, const struct discord_voice_state *vs) {
-    if (vs->channel_id) {
+    if (vs->deaf || vs->mute || vs->self_deaf || vs->self_mute || vs->self_stream || vs->self_video || vs->supress) return;
+    else if (vs->channel_id) {
         switch (vs->channel_id) {
             case VC_CHAT_ONE: discord_add_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_CHAT_ONE); break;
             case VC_CHAT_TWO: discord_add_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_CHAT_TWO); break;
@@ -10,13 +11,14 @@ void on_voice_state_update(struct discord *client, const struct discord_user *bo
             default: break;
         }
     }
-
-    for (size_t i=0; vs->member->roles[i]; i++) {
-        switch (vs->member->roles[i]->value) {
-            case R_VC_CHAT_ONE: discord_remove_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_CHAT_ONE); break;
-            case R_VC_CHAT_TWO: discord_remove_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_CHAT_TWO); break;
-            case R_VC_MUSIC: discord_remove_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_MUSIC); break;
-            default: break;
+    else {
+        for (size_t i=0; vs->member->roles[i]; i++) {
+            switch (vs->member->roles[i]->value) {
+                case R_VC_CHAT_ONE: discord_remove_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_CHAT_ONE); break;
+                case R_VC_CHAT_TWO: discord_remove_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_CHAT_TWO); break;
+                case R_VC_MUSIC: discord_remove_guild_member_role(client, vs->guild_id, vs->user_id, R_VC_MUSIC); break;
+                default: break;
+            }
         }
     }
 
