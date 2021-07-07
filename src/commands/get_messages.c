f6eea6adf5677d9396982a263b2e42d72226b756
Synth <synth9283@gmail.com>
2021-07-07 16:19:43 -0400

    Fixed command code

--- src/commands/get_messages.c
+++ src/commands/get_messages.c
@@ -11,35 +11,48 @@ void get_messages(struct discord *client, const struct discord_user *bot, const 
     struct discord_embed *embed = discord_embed_alloc();
     struct discord_create_message_params params = {.embed = embed};
 
-    if (rc) failed_message(embed, &params, sqlite3_errmsg(errMsg), msg->channel_id);
+    if (rc) failed_message(client, embed, &params, (char*)sqlite3_errmsg(db), msg->channel_id);
 
-    rc = sqlite3_exec(db, ".headers on", NULL, &errMsg);
+    rc = sqlite3_exec(db, ".headers on", NULL, NULL, &errMsg);
 
-    if (rc != SQLITE_OK) failed_message(embed, &params, sqlite3_errmsg(errMsg), msg->channel_id);
+    if (rc != SQLITE_OK) {
+        failed_message(client, embed, &params, errMsg, msg->channel_id);
+        sqlite3_free(errMsg);
+    }
     else {
-        rc = sqlite3_exec(db, ".mode csv", NULL, &errMsg);
+        rc = sqlite3_exec(db, ".mode csv", NULL, NULL, &errMsg);
 
-        if (rc != SQLITE_OK) failed_message(embed, &params, sqlite3_errmsg(errMsg), msg->channel_id);
+        if (rc != SQLITE_OK) {
+            failed_message(client, embed, &params, errMsg, msg->channel_id);
+            sqlite3_free(errMsg);
+        }
         else {
             sqlite3_mprintf(".output %s.csv", msg->author->username);
-            rc = sqlite3_exec(db, ".output ", NULL, &errMsg);
+            rc = sqlite3_exec(db, ".output ", NULL, NULL, &errMsg);
 
-            if (rc != SQLITE_OK) failed_message(embed, &params, sqlite3_errmsg(errMsg), msg->channel_id);
+            if (rc != SQLITE_OK) {
+                failed_message(client, embed, &params, errMsg, msg->channel_id);
+                sqlite3_free(errMsg);
+            }
             else {
-                query = sqlite3_mprintf("SELECT timestamp, author_id, message_id, content FROM %s WHERE message_id = %lu;", MESSAGE_TABLE, message_id);
-                rc = sqlite3_exec(db, query, NULL, &errMsg);
+                query = sqlite3_mprintf("SELECT timestamp, author_id, message_id, content FROM %s WHERE message_id = %lu;", MESSAGE_TABLE, msg->id);
+                rc = sqlite3_exec(db, query, NULL, NULL, &errMsg);
 
-                if (rc) {
+                if (rc != SQLITE_OK) {
+                    failed_message(client, embed, &params, errMsg, msg->channel_id);
+                    sqlite3_free(errMsg);
+                }
+                else {
                     query = sqlite3_mprintf(".output %s.csv", msg->author->username);
 
                     size_t filename_len = strlen(query);
                     embed->color = COLOR_MINT;
                     snprintf(embed->title, 257, "Sent!");
-                    snprintf(embed->description, 2049, "Please check your dms from the bot: %s", sqlite3_errmsg(errMsg));
+                    snprintf(embed->description, 2049, "Please check your dms from the bot: %s", errMsg);
                     params.file.name = sqlite3_mprintf("%s.csv", msg->author->username);
 
                     discord_create_dm(client, msg->author->id, dm_channel);
-                    discord_create_message(client, dm_channel, &params, NULL);
+                    discord_create_message(client, dm_channel->id, &params, NULL);
 
                     remove(params.file.name);
                 }
