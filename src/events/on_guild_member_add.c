51167118540d69fac0af36dd558b3c47c429ebb6
Synth <synth9283@gmail.com>
2022-01-17 19:31:40 -0500

    fix: on_guild_member_add

--- src/events/on_guild_member_add.c
+++ src/events/on_guild_member_add.c
@@ -2,7 +2,7 @@
 #include "../libs/bot_include.h"
 
 void on_guild_member_add(struct discord *client, const u64_snowflake_t guild_id, const struct discord_guild_member *member) {
-    char username_and_discriminator[DISCORD_MAX_USERNAME_LEN], user_id_str[ID_STR_LEN], timestamp_str[TIMESTAMP_NORMAL_STR_LEN];
+    char username_and_discriminator[DISCORD_MAX_USERNAME_LEN], user_id_str[ID_STR_LEN], timestamp_str[TIMESTAMP_NORMAL_STR_LEN], footer_text[ID_STR_LEN+4];
     char *avatar_url = malloc(AVATAR_URL_LEN), *user_mention_str = malloc(USER_MENTION_LEN);
     struct discord_embed embed;
     discord_embed_init(&embed);
@@ -23,7 +23,7 @@ void on_guild_member_add(struct discord *client, const u64_snowflake_t guild_id,
     if (member->user->bot) {
         discord_add_guild_member_role(client, guild_id, member->user->id, R_BOT);
 
-        snprintf(embed.title, sizeof(embed.title), "New bot %s", username_and_discriminator);
+        discord_embed_set_title(&embed, "New bot %s", username_and_discriminator);
         discord_embed_add_field(&embed, "Bot ID", user_id_str, true);
         discord_embed_add_field(&embed, "Bot", user_mention_str, true);
     }
@@ -36,14 +36,15 @@ void on_guild_member_add(struct discord *client, const u64_snowflake_t guild_id,
         discord_add_guild_member_role(client, guild_id, member->user->id, R_FUN);
         discord_add_guild_member_role(client, guild_id, member->user->id, R_OTHER);
 
-        snprintf(embed.title, sizeof(embed.title), "Welcome %s!", username_and_discriminator);
-        snprintf(embed.description, sizeof(embed.description), "Welcome **%s** to PD! Please checkout <#%lu> and <#%lu> to get started!", user_mention_str, C_SERVER_INFO, C_ROLES);
-        snprintf(embed.footer->text, sizeof(embed.footer->text), "ID: %lu", member->user->id);
+        discord_embed_set_title(&embed, "Welcome %s!", username_and_discriminator);
+	discord_embed_set_description(&embed, "Welcome **%s** to PD! Please checkout <#%lu> and <#%lu> to get started!", user_mention_str, C_SERVER_INFO, C_ROLES);
+        snprintf(footer_text, sizeof(footer_text), "ID: %lu", member->user->id);
+	discord_embed_set_footer(&embed, footer_text, avatar_url, NULL);
 
         discord_create_message(client, C_WELCOME, &params, NULL);
 
-        snprintf(embed.title, sizeof(embed.title), "New user %s", username_and_discriminator);
-        snprintf(embed.description, 1, "");
+        discord_embed_set_title(&embed, "New user %s", username_and_discriminator);
+	discord_embed_set_description(&embed, "");
         discord_embed_add_field(&embed, "User ID", user_id_str, true);
         discord_embed_add_field(&embed, "User", user_mention_str, true);
     }
